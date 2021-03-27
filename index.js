b0dd40fe17e18721b38576ec86925978bc996b9f
fmarcos <fmarcosdev@gmail.com>
2021-03-26 21:03:27 -0300

    comentarios

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
 // 1. Composite function
 var rokket = (dataA, dataB, dataC) => {
-    return (dataA * dataB * dataC)
+    return (dataA * dataB * dataC) /*return values*/
 }
 
 const a = 8, b = 2, c = 1;
-console.log(rokket(a, b, c))
+console.log(rokket(a, b, c)) /*result*/
 
 
 // 2. Longest string
 var rokket = (list) => {
     var longestString = undefined;
-    for (var i = 0; i < list.length; i++) {
-        longestString == undefined ? longestString = list[i]
-            : list[i].length > longestString.length ? longestString = list[i] : null;
+
+    for (var i = 0; i < list.length; i++) { /*for each element*/
+        longestString == undefined ? longestString = list[i] /*asign varaible*/
+            : list[i].length > longestString.length ? longestString = list[i] : null; /*check the longest string*/
     }
-
-    return longestString;
+    return longestString; /*return values*/
 }
-
 const list = ['best', 'company', 'ever']
 console.log(rokket(list))
 
 //3. String repetition
 var rokket = (someString, times) => {
     var resSomeString = '';
-    while (times > 0) {
-        resSomeString += someString
-        times--
+    while (times > 0) { /*check varaible*/
+        resSomeString += someString /*add conetent*/
+        times--/*decrease times*/
     }
-    return resSomeString
+    return resSomeString /*return values*/
 }
 
 console.log(rokket('palabra', 10))
 
 // 4. Only last names
 var rokket = (someListObj) => {
-    let resultado = someListObj.map((a) => {
-        return a.lastName
+    let resultado = someListObj.map((a) => { /*for each element*/
+        return a.lastName /*set'resultado' value*/
     })
-    return resultado
+    return resultado  /*return values*/
 
 }
 
@@ -50,9 +49,9 @@ const contacts = [
 console.log(rokket(contacts))
 
 // 5. Unique numbers   
-var rokket = (listA, listB) => {
-    var arrResult = listA.concat(listB)
-    return [...new Set(arrResult)]
+var rokket = (listA, listB) => { 
+    var arrResult = listA.concat(listB) /*combine 2 arrays*/
+    return [...new Set(arrResult)]  /*return the uniques in array*/
 
 }
 
